b1ed854599dfd311b36f2813df3469eca27381ca
Jessica Willan <j-willan@hotmail.co.uk>
2023-02-20 11:39:40 +0000

    this commit will add updated README file, updated index file, updated package.json file

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,4 +1,10 @@
-exports.categoryData = require('./categories.js');
-exports.commentData = require('./comments.js');
-exports.reviewData = require('./reviews.js');
-exports.userData = require('./users.js');
+// exports.categoryData = require('./categories.js');
+// exports.commentData = require('./comments.js');
+// exports.reviewData = require('./reviews.js');
+// exports.userData = require('./users.js');
+exports.data = {
+  categoryData: require('./categories.js'),
+  reviewData: require('./reviews.js'),
+  userData: require('./users.js'),
+  commentData: require('./comments.js'),
+};
