a8b910224836dc891d2823d4006f427ab1541980
Jessica Willan <j-willan@hotmail.co.uk>
2023-02-23 14:09:36 +0000

    this commit will add length check and specift expected review_id to GET comments by review id 200 test. Also adds second 200 test for an empty array when review_id is valid but there are no associated comments.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -96,23 +96,34 @@ describe('app', () => {
         });
     });
   });
-  describe.only('GET /api/reviews/:review_id/comments', () => {
+  describe('GET /api/reviews/:review_id/comments', () => {
     test('200: GET should return an array of comment objects when passed a valid review_id', () => {
       return request(app)
         .get(`/api/reviews/2/comments`)
         .expect(200)
         .then(({body}) => {
           expect(Array.isArray(body.comments)).toBe(true);
+          expect(body.comments).toHaveLength(3)
           body.comments.forEach((comment) => {
             expect(comment).toHaveProperty('comment_id', expect.any(Number));
             expect(comment).toHaveProperty('votes', expect.any(Number));
             expect(comment).toHaveProperty('created_at', expect.any(String));
             expect(comment).toHaveProperty('author', expect.any(String));
             expect(comment).toHaveProperty('body', expect.any(String));
-            expect(comment).toHaveProperty('review_id', expect.any(Number));
+            expect(comment.review_id).toBe(2);
           });
         });
     });
+    test('200: GET should return an empty comments array when passed a valid review_id that has no comments', () => {
+      return request(app)
+      .get('/api/reviews/5/comments')
+      .expect(200)
+      .then(({body}) => {
+        expect(Array.isArray(body.comments)).toBe(true);
+        expect(body.comments).toHaveLength(0)
+      })
+    })
+
     test('400: GET should return an error message when queried with an invalid review_id', () => {
       return request(app)
         .get('/api/reviews/notanid/comments')
